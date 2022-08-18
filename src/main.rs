0549ea20aa13f91231a70eeae26ae01715092e45
megahomyak <g.megahomyak@gmail.com>
2022-08-18 19:59:13 +0500

    I've got my first block, wahoo!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let blockchain_client =
+        web3::Web3::new(web3::transports::http::Http::new("https://eth.public-rpc.com").unwrap());
+    let block_id = web3::types::BlockId::Number(web3::types::BlockNumber::Number(
+        blockchain_client.eth().block_number().await.unwrap(),
+    ));
+    let block = blockchain_client
+        .eth()
+        .block(block_id)
+        .await
+        .unwrap()
+        .unwrap();
+    println!("{:?}", block);
 }
